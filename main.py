a8ded825182d5e519799e36858907d3892acddce
Aoife1 <74499497+Missyor@users.noreply.github.com>
2022-02-18 08:35:50 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,33 @@
+Q1
+Write a program that creates a variable that asks that asks a user to enter 10 ages in a random order. Call the variable ageInput.
+
+Q2
+Create a csv file called ages that contains the information stored in ageInput and close the file.
+
+Q3
+Open the csv file you created
+Read it into a new variable called dataIn.
+Convert the contents of dataIn to a list.
+Print the list.
+
+Q4
+Sort the list into ascending order and print it. (if you couuld not create the list from the csv file - create a list now and sort it)
+
+Q5
+Sort the list into ascending order and print it. (if you couuld not create the list from the csv file - create a list now and sort it) 
+
+Q6
+posList = [-1, 3, -4, 5]
+Remove all negative numbers from the list and print the outcome
+
+Q7
+avgList = [1, 2,3, 4, 5]
+Using pythons in-built functions, find the mean, median and mode of avgListPrint each outcome.
+
+Q8
+numbers = [1, 2, 4, 3, 5, 2]
+names = ["a", "b", "c", "d", "e", "f"]
+Create a barchart to display the data. 
+Label the x-axis as names and the y-axis as pets.
+Give your graph the title of Number of pets.
+Display your graph
